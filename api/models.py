2d1b617af3bbcddf7daeb5bc3c45d531421d2d36
Волков Дмитрий Алексеевич <volkovda@pochtabank.ru>
2020-09-29 22:05:46 +0300

    merged

--- api/models.py
+++ api/models.py
@@ -1,3 +1,96 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Category(models.Model):
+    '''Категории (типы) произведений'''
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+
+class Genre(models.Model):
+    '''Жанры'''
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+
+class Title(models.Model):
+    '''Название произведения (фильма/книги/музыка)'''
+    name = models.CharField(max_length=90)
+    year = models.IntegerField()
+    description = models.TextField(
+        max_length=200,
+        blank=True
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        related_name='genre'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        related_name='categories'
+    )
+
+
+class Review(models.Model):
+    '''Отзывы пользователе о произведении'''
+    SCORE_CHOICES = zip(range(1, 11), range(1, 11))
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    score = models.IntegerField(choices=SCORE_CHOICES, default=1)
+    pub_date = models.DateTimeField(
+        'Дата публикации отзыва',
+        auto_now_add=True,
+    )
+
+
+class User(AbstractUser):
+
+    class Role(models.TextChoices):
+        USER = 'user'
+        MODERATOR = 'moderator'
+        ADMIN = 'admin'
+
+    email = models.EmailField(('email address'), blank=False, unique=True)
+    bio = models.TextField(blank=True)
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.USER,
+        )
+    confirmation_code = models.CharField(max_length=100, blank=True, )
+
+    def __str__(self):
+        return self.username
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации комментария',
+        auto_now_add=True
+    )
