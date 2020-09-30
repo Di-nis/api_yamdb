58c9bb04038d15538dc09989f06a35e20e9695fe
yaroslav194 <yaroslav_levchuk@mail.ru>
2020-09-30 23:16:47 +0300

    11

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
-from users import views
+from .users import views
 
 from .views import UserListCreate
 
