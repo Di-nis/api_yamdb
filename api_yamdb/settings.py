559b8d9a949f763958e9a32ba4c6016690e66de2
yaroslav194 <yaroslav_levchuk@mail.ru>
2020-09-30 18:18:56 +0300

    reg_mail

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'api',
     'titles',
     'users',
+    
 ]
 
 MIDDLEWARE = [
--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,5 @@
 from rest_framework import serializers
-
 from .models import User
-
-# from rest_framework.fields import CurrentUserDefault
-# from rest_framework.validators import UniqueTogetherValidator
-
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,3 +7,14 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+
+class EmailSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+
+class EmailCodeSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    confirmation_code = serializers.CharField(max_length=32)
+
+
