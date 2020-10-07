17db2dc0176df5fc8d90239455d5c47137981a97
yaroslav194 <yaroslav_levchuk@mail.ru>
2020-10-07 17:09:46 +0300

    review_views

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -123,6 +123,7 @@ EMAIL_HOST_USER = 'denis.smirnov1985@gmail.com'
 # EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_HOST_PASSWORD = 'Dynamo1923'
 EMAIL_PORT = 587
+DOMAIN_NAME = 'yamdb@example.com'
 
 
 SIMPLE_JWT = {
